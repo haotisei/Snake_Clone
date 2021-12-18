817fcf276db4e02243936bdb8f9c1e107271c510
haotisei <96281130+haotisei@users.noreply.github.com>
2021-12-18 23:27:58 +0900

    create basic stage objects

---
+++ Assets/Scripts/SnakeMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeMovement : MonoBehaviour
+{
+    public float ForwardSpeed = 5;
+    public float Sensitivity = 10;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
