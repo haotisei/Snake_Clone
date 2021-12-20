51297fe1166b68011a3f8d508cdb7f12e2ac016b
haotisei <96281130+haotisei@users.noreply.github.com>
2021-12-20 20:45:11 +0900

    set blocks for level1

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -28,22 +28,6 @@ public class Block : MonoBehaviour
 
     void Start()
     {
-         /*g = new Gradient();
-        gck = new GradientColorKey[2];
-        gck[0].color = Color.red;
-        gck[0].time = 0.0f;
-        gck[1].color = Color.black;
-        gck[1].time = 1.0f;
-
-        gak = new GradientAlphaKey[2];
-        gak[0].alpha = 1.0f;
-        gak[0].time = 1.0f;
-        gak[1].alpha = 1.0f;
-        gak[1].time = 1.0f;
-
-        g.SetKeys(gck, gak);
-         */
-
         color = (float)(health) / 50;
         Debug.Log(color);
 
